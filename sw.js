8cef926771e22782f53b53e82f1edff52772bd35
matheusm-png <matheus.m@yourh.com.br>
2026-04-22 17:40:57 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -17,6 +17,14 @@ self.addEventListener('install', event => {
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(cachedResponse => {
+        const fetchPromise = fetch(event.request).then(networkResponse => {
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, networkResponse.clone());
+          });
+          return networkResponse;
+        });
+        return cachedResponse || fetchPromise;
+      })
   );
 });
