5f704f42c0820d0359333abb790a392147d31a95
matheusm-png <matheus.m@yourh.com.br>
2026-04-22 17:37:30 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = '8em12-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/script.js',
+  '/assets/logo/logo.webp',
+  '/assets/logo/8em12logo_hero.webp'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
